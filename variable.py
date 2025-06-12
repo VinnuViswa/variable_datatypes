935395d9747a759acac3e6325d372635f4cfa9b8
BandaruViswatejaGoud18 <vinnuviswa7@gmail.com>
2025-06-12 20:50:51 +0530

    today we completed the variables and these are the notes of it

---
+++ variable.py
@@ -0,0 +1,96 @@
+# VARIABLES:-
+
+    # Rules to create variables in python:-
+    
+        # 1) To create a variable you can use alphabets [A-Z, a-z]
+        
+        # 2) To create a variable you can use numbers [0-9], but do not use numbers at start of the variable.
+                # room403 ---> OK
+                # 403room ---> NOT OK
+                # room403class ---> OK
+                
+        # 3) To create a variable you can not use special charecters except _
+            # Ex: !@#$%^&*(){}+=:;""><?/|\
+                
+        # 4) To create a variable you can not use a keyword.
+             # what is mean by keyword ?
+                # A word which is being used by pythhon itself to operate internally.
+                
+# import keyword
+# print(keyword.kwlist)
+
+# List of Keyword :-
+
+'False', 'None', 'True', 'and', 'as', 'assert', 
+'async', 'await', 'break', 'class', 'continue', 
+'def', 'del', 'elif', 'else', 'except', 'finally', 
+'for', 'from', 'global', 'if', 'import', 'in', 'is', 
+'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 
+'return', 'try', 'while', 'with', 'yield'
+
+##############PRACTICE OF VARIABLES#######################
+
+name = 'kumar'
+
+NAME = 'Shiv'
+
+Name = 'Shivkuamr'
+
+MyName = 'Shivkumar'
+
+FrUIt = 'Apple'
+
+roll_No = 34
+
+_city = 'Hyderabad'
+
+# 5_times = 'Eating'
+
+eat_5_times = True
+
+# @Python = 'Good'
+
+myage_ = 32
+
+##########################
+# Always create a variable which is meaningful and easy to understand.
+# it should define truly the value / data stored in it.
+
+# sfsf = 78
+
+# pi = 3.14
+
+# todays_date = 30
+
+# current_year = 2025
+
+# weight_kg = 55
+
+
+########################################
+
+# Comments
+
+# This programme is for finding even numbers from list
+
+# Do not change the logic, it will affect other programmes
+
+
+'''Hello, How are you ?
+  This is shiv from hyderbad.
+  I am going to Delhi tomorrow
+
+'''
+
+"""Hello, Ho are you ?
+   Good morning.
+"""
+
+###############################
+
+
+# def greet(name='Buddy'):
+#     return f"Hello {name}"
+
+# x = greet('Shiv')
+# print(x)
