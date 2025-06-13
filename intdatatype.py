bf3f1c4819cf287f83681ec27631544f0a5bfdf3
VinnuViswa <bandaruviswatejagoud@gmail.com>
2025-06-13 10:03:31 +0530

    Today we learnt about Numeric datatype and its typesalong with Arithematic operation and these are the notes for it

---
+++ intdatatype.py
@@ -0,0 +1,95 @@
+# Numeric datatypes :-
+
+ #1) INT data type :-
+    #An interger is a number without decimal point
+    #0 is also an interger
+    #positive and negative numbers can be integers.
+# Examples:-
+        # 1, 0, -45, 100 YES 
+        # -509.000 NO 
+# 2) Float data type :-
+    # A float is a number with decimal point
+    # A float can be +ve and -ve 
+#Examples :-
+        #0.0, 34.5555550, 90.50, -1.456
+        
+#3) Complex Number data type :-
+    # A complex number is a combination of real number and imaginary number
+        # complex = real +/- imaginary
+    #What is meant by reak number :-
+        # All ints and floats are real numbers
+            # Ex :- 4, -5, 9.0, -23.55, 0.56
+            
+    # What is imaginary number :-
+     # A number which comes whith prefix 'j' is called imaginary number
+        # Ex :- 3j, -8j, 90.56j, -12,23j
+        
+# Examples of complex :-
+    # 6+5j, -34-50j, 12-45j, 10.5+50.45j, -90.78-0.56j
+
+
+#x = 5
+#y = -2
+#print(type(x))
+#print(type(y))
+
+#m = 45.50
+#n = -67.34
+#print(type(m))
+#print(type(n))
+
+#p = 5 - 7j
+#q = -5.6 + 8.345j
+#print(type(p))
+#print(type(q))
+
+########################
+
+# Addition +
+
+# num_1 = 15
+# num_2 = 5
+
+# sum = num_1 + num_2
+# print(sum)
+
+# Subraction -
+# sub = num_1 - num_2
+# print(sub)
+
+#Multiplication
+
+# mul = num_1 * num_2
+# print(mul)
+
+# Simple Division \
+    
+# s_div =  num_1/num_2
+# print(s_div)
+
+# // Floor division
+# print(33 / 15)
+# print(33 // 15)
+
+# p = 7
+# q = 4
+
+# Mod operation % (remainder after floor division)
+
+# print(p % q)
+
+# print(45 % 20)
+
+# Exponent **
+
+print(3 ** 3)
+
+print(5 ** 8)
+
+# Take all Numeric data types and do arithematic operations
+
+x = 56.5
+y = 13.2
+
+m = 24 + 6j
+n = 10 + 3j
