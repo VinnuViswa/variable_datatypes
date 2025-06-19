e0c203e5906b436ff326d3f829e55a7276d6ecaa
VinnuViswa <bandaruviswatejagoud@gmail.com>
2025-06-19 17:49:35 +0530

    Boolean

--- 4booleandatatype.py
+++ 4booleandatatype.py
@@ -1,13 +1,57 @@
-# What is boolean Data Type :-
+# What is boolean Data Type:-
     # True  ----> 1
-    # Flase ---> 0
+    # False  ----> 0
     
-# Arithmetic Operation on Booleans :-
-print(True + True + False)
-print(True + True + False*(True + True))
+# Arithmetic Operation on Booleans:-
+# print(True + True + False *(True + True))
 
-x = 5
-print(x == 5)
-print(x == 15)
+# x = 5
+# print(x == 15)
 
+# y = True
+
+# print(y * y)
+
+##########################################
+
+# str1 = 'Anaconda is a big snake'
+
+# print(str1[::-1])
+# print(str1[-1::-1].upper())
+# string concatination:-
+# str2 = str1[3:5:1] + str1[-2::]
+# print(str2)
+# # # SINA
+# # str3 = str1[10:8:-1].upper() + str1[1:3:1].upper()
+# # print(str3)
+
+##############################
+
+# str4 = '56789'
+# print(int(str4[0]) * int(str4[1]) * int(str4[2]) * int(str4[3]) * int(str4[4]))
+
+
+# x = 5
+
+# y = float(x)
+# print(y)
+
+# z = complex(x)
+# print(z)
+
+
+# a = '10'
+
+# print(float(a))
+
+# m = 'hari'
+# print(int(m))  #ValueError: invalid literal for int() with base 10: 'hari'
+
+z = 'krish'
+
+# print(z * 5)
+# print('krishkrishkrish'/2)
+print(ord('$'))
+
+print(bin(65))
     
